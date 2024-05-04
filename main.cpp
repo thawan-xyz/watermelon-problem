caa0a009551df6a0cf07f5b0d184b129f8a30440
thawan-xyz <thawan.xyz@gmail.com>
2024-05-04 17:53:45 -0300

    end

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int weight;
+    cin >> weight;
+
+    if (!(weight % 2) && (weight != 2)) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
